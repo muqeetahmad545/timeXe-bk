6656d00ea8aa2dfa5883f37d221c527ca7bcd275
Muqeet Ahmad <inoxentuni1@gmaill.com>
2024-06-25 21:16:02 +0500

    working

--- src/controllers/helpers/cloudinary.helper.ts
+++ src/controllers/helpers/cloudinary.helper.ts
@@ -1,45 +1,3 @@
-// import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// cloudinary.config({
-//   cloud_name: process.env.CLOUD_NAME || "",
-//   api_key: process.env.CLOUD_API_KEY || "",
-//   api_secret: process.env.CLOUD_API_SECRET || "",
-// });
-
-// interface UploadOptions {
-//   resource_type: "auto";
-//   public_id: string;
-//   folder: string;
-// }
-
-// export const handleUpload = async (file: Express.Multer.File, folderName: string): Promise<UploadApiResponse> => {
-//   try {
-//     // console.log("Cloudinary Configuration:");
-//     // console.log("cloud_name:", process.env.CLOUD_NAME);
-//     // console.log("api_key:", process.env.CLOUD_API_KEY);
-//     // console.log("api_secret:", process.env.CLOUD_API_SECRET);
-
-//     const uploadOptions: UploadOptions = {
-//       resource_type: "auto",
-//       public_id: file.originalname,
-//       folder: folderName,
-//     };
-
-//     console.log("Uploading file:", file.originalname);
-//     const res = await cloudinary.uploader.upload(file.path, uploadOptions);
-//     console.log("File upload successful. Cloudinary response:", res);
-
-//     return res as UploadApiResponse;
-//   } catch (error: any) {
-//     console.error(`Error uploading image to Cloudinary: ${error.message}`);
-//     throw new Error(`Error uploading image to Cloudinary: ${error.message}`);
-//   }
-// };
-
-
 
 import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
 import dotenv from "dotenv";
@@ -48,9 +6,9 @@ import { Readable } from "stream";
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME || "",
-  api_key: process.env.CLOUD_API_KEY || "",
-  api_secret: process.env.CLOUD_API_SECRET || "",
+  cloud_name: process.env.CLOUD_NAME || "dfbsbullu",
+  api_key: process.env.CLOUD_API_KEY || "551785228467189",
+  api_secret: process.env.CLOUD_API_SECRET || "A1Ytz_3SebjVv52aDnrMFYuxN",
 });
 
 interface UploadOptions {
