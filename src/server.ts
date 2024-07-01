35829c78e7b40694c96618a40090598a8576a28a
Muqeet Ahmad <inoxentuni1@gmaill.com>
2024-07-01 15:15:17 +0500

    working

--- src/server.ts
+++ src/server.ts
@@ -31,17 +31,17 @@ app.use(bodyParser.json());
 app.use("/api", userRoutes);
 
 // cron.schedule('36 20 * * *', async () => {
-cron.schedule(
-  "58 20 * * *",
-  // "0 14 * * *",
-  async () => {
-    await markAbsentees();
-  },
-  {
-    timezone: "Asia/Karachi",
-  }
-);
-console.log("Attendance checker scheduled.");
+// cron.schedule(
+//   "58 20 * * *",
+//   // "0 14 * * *",
+//   async () => {
+//     await markAbsentees();
+//   },
+//   {
+//     timezone: "Asia/Karachi",
+//   }
+// );
+// console.log("Attendance checker scheduled.");
 app.use(express.static(path.join(__dirname, "../build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../build", "index.html"));
